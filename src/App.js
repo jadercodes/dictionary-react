425acd339f8ac36eeb825ab7b2fb7d9c251928b3
jadercodes <97782554+jadercodes@users.noreply.github.com>
2022-06-25 12:17:23 +0700

    add search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Dictionary />
+        <footer>
+          Open source code on{" "}
+          <a
+            className="git"
+            href="https://github.com/jadercodes/dictionary-react"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          autofocus={true}
+          onChange={handleKeywordChange}
+        ></input>
+      </form>
+    </div>
+  );
+}
